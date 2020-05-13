6ad30984a661dd9ffd59bdfc5addef98ee455fde
Umesh Chaudhary <umesschaudhary@gmail.com>
2020-05-13 22:21:14 +0545

    added to env sample

---
+++ config/settings/env.sample.py
@@ -0,0 +1,25 @@
+from .base import *
+
+ALLOWED_HOSTS = ['*']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blogic',
+        'USER': 'blogicadmin',
+        'PASSWORD': 'blogic123',
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'CONN_MAX_AGE': 600,
+    }
+}
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+    'drf_yasg',
+    'django_extensions'
+)
+
+MIDDLEWARE += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
