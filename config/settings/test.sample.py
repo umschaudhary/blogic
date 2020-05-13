04e6dd5df37b32f104fa984dbf3d9239405898c0
Umesh Chaudhary <umesschaudhary@gmail.com>
2020-05-13 17:12:13 +0545

    add gitlab ci/cd

--- config/settings/test.sample.py
+++ config/settings/test.sample.py
@@ -5,10 +5,10 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'jobportal3',
-        'USER': 'jobadmin',
-        'PASSWORD': 'admin123',
-        'HOST': 'localhost',
+        'NAME': 'ci',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': 'postgres',
         'PORT': '5432',
         'CONN_MAX_AGE': 600,
     }
