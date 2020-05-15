dfe898f87ff9a51850416c52bf8af1b2ec61143e
Umesh Chaudhary <umesschaudhary@gmail.com>
2020-05-16 01:22:15 +0545

    add db on gitlab yml

--- config/settings/test.sample.py
+++ config/settings/test.sample.py
@@ -6,7 +6,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ci',
-        'USER': 'postgres',
+        'USER': 'runner',
         'PASSWORD': '',
         'HOST': 'postgres',
         'PORT': '5432',
