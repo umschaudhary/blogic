59f231fcf7d45f256e75c01843b31da7b880890c
Umesh Chaudhary <umesschaudhary@gmail.com>
2020-05-17 00:04:20 +0545

    add phone otp to admin.py

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from apps.users.models import User
+from apps.users.models import User, PhoneOtp
 
-admin.site.register(User)
+admin.site.register([User, PhoneOtp])
