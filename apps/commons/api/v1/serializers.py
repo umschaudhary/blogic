df3524a283fa2dc736c5604a7f8760790c124342
Umesh Chaudhary <umesschaudhary@gmail.com>
2020-05-25 11:11:22 +0545

    add unique validator on categories

--- apps/commons/api/v1/serializers.py
+++ apps/commons/api/v1/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from apps.api.v1.mixins.serializer import DynamicFieldsModelSerializer
 from apps.commons.utils.validators import validate_name
 from apps.commons.models.common import Category
 
 
 class CategorySerializer(DynamicFieldsModelSerializer):
-    name = serializers.CharField(max_length=50, validators=[validate_name])
+    name = serializers.CharField(
+        max_length=50, validators=[
+            validate_name, UniqueValidator(queryset=Category.objects.all())]
+    )
 
     class Meta:
         model = Category
