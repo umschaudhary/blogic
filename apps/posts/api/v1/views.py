06e41228003acee5d06feaf1d03d0983627bf2e2
Umesh Chaudhary <umesschaudhary@gmail.com>
2020-05-25 11:30:40 +0545

    increate view_count on post detail view

--- apps/posts/api/v1/views.py
+++ apps/posts/api/v1/views.py
@@ -1,6 +1,7 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.response import Response
 
 from apps.api.v1.mixins.viewset import CreateListRetrieveUpdateViewSet
 from apps.posts.models import Post
@@ -43,3 +44,10 @@ class PostViewSet(CreateListRetrieveUpdateViewSet):
         except KeyError:
             # action is not set return default permission_classes
             return [permission() for permission in self.permission_classes]
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        instance.view_count += 1
+        instance.save()
+        return Response(serializer.data)
